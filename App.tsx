2d422d11a2bf02a97c860b32d434008dcc776024
Yooshin Kim <yusinkim.or@gmail.com>
2024-08-02 01:37:21 +0900

    feat : 인스타그램 공유 연결

--- App.tsx
+++ App.tsx
@@ -3,8 +3,11 @@ import CameraRoll from '@react-native-community/cameraroll';
 import React from 'react';
 import {Alert, Platform, SafeAreaView, StyleSheet} from 'react-native';
 import WebView, {WebViewMessageEvent} from 'react-native-webview';
+import Share from 'react-native-share';
 
 function App(): React.JSX.Element {
+  const webviewRef = React.useRef<WebView>(null);
+
   const onMessageFromWebView = ({nativeEvent}: WebViewMessageEvent) => {
     const {type, data} = JSON.parse(nativeEvent.data);
     console.log(type, data);
@@ -13,7 +16,7 @@ function App(): React.JSX.Element {
       copyText(data.text);
       downloadImage(data.url);
     } else if (type === 'shareInsta') {
-      shareInsta();
+      shareInsta(data.filePath);
     }
   };
 
@@ -22,10 +25,22 @@ function App(): React.JSX.Element {
     CameraRoll.save(url, {type: 'photo'})
       .then(value => {
         console.log(value);
+        webviewRef.current?.postMessage(
+          JSON.stringify({
+            type: 'saveAllResult',
+            data: {success: true, imagePath: value},
+          }),
+        );
         Alert.alert('이미지 저장에 성공하였습니다. ');
       })
       .catch(e => {
         console.error(e);
+        webviewRef.current?.postMessage(
+          JSON.stringify({
+            type: 'saveAllResult',
+            data: {success: false, imagePath: ''},
+          }),
+        );
         Alert.alert('이미지 저장에 실패하였습니다. ');
       });
   }
@@ -35,11 +50,24 @@ function App(): React.JSX.Element {
     Alert.alert('클립보드에 복사되었습니다.');
   }
 
-  function shareInsta() {}
+  function shareInsta(filePath: string) {
+    Share.shareSingle({
+      social: Share.Social.INSTAGRAM,
+      url: `file://${filePath}`,
+      type: 'image/*',
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        err && console.log(err);
+      });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <WebView
+        ref={webviewRef}
         source={{uri: 'http://169.254.253.131:5173/'}}
         onMessage={onMessageFromWebView}
         userAgent={`sodong_${Platform.OS}`}
